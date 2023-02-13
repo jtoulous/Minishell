c401f35998b311a1befee0dc2e5145c59350e061
Axel Goichon <agoichon@3A5.42angouleme.fr>
2023-02-13 08:34:55 +0100

    push_A23

--- error_management.c
+++ error_management.c
@@ -6,7 +6,7 @@
 /*   By: agoichon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 16:33:08 by agoichon          #+#    #+#             */
-/*   Updated: 2023/02/10 11:20:09 by agoichon         ###   ########.fr       */
+/*   Updated: 2023/02/13 08:34:27 by agoichon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 int	error_inredir(char *failed_redir)
 {
 	ft_putstr_fd(failed_redir, STDOUT_FILENO);
-	ft_putstr_fd(": No such file or directory", STDOUT_FILENO);
+	ft_putstr_fd(": No such file or directory\n", STDOUT_FILENO);
 	free (failed_redir);
 	return (0);
 }
@@ -25,12 +25,12 @@ int	error_inredir(char *failed_redir)
 void	error_quotes(t_data *data)
 {
 	data->exec_stat = 0;
-	ft_putstr_fd("va niquer ta race a pas fermer tes putains de quotes", STDOUT_FILENO);
+	ft_putstr_fd("va niquer ta race a pas fermer tes putains de quotesi\n", STDOUT_FILENO);
 }
 
 void	error_path(char *cmd)
 {
 	ft_putstr_fd(cmd, 1);
-	ft_putstr_fd(": command not found", 1);
+	ft_putstr_fd(": command not found\n", 1);
 	free (cmd);
 }
