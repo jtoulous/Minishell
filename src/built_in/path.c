ad4478997ce0579f8504880aa0f0599af6ba8e8a
Joshua Toulouse <jtoulous@1D6.42angouleme.fr>
2023-03-15 15:53:10 +0100

    wesh

---
+++ src/built_in/path.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agoichon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 09:27:32 by agoichon          #+#    #+#             */
+/*   Updated: 2023/03/15 14:27:48 by agoichon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+void	built_in_pwd(t_data *data)
+{
+	char	cwd[PATH_MAX];
+
+	(void) data;
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+		ft_putstr_fd("Error getting current working directory\n", 1);
+	else
+		printf("%s\n", cwd);
+}
+
+void	built_in_env(t_data *data)
+{
+	int	z;
+	int	y;
+
+<<<<<<< HEAD
+	z = 0;
+	if (!data->envp[0])
+		return;
+=======
+	z = 1;
+>>>>>>> 37e258b60fbd0ef11561946012444c0df385998e
+	while (data->envp[z])
+	{
+		y = 0;
+		while (data->envp[z][y])
+		{
+			if (data->envp[z][y] == '=')
+			{	
+				printf("%s\n", data->envp[z]);
+				break ;
+			}
+			if (data->envp[z][y] == '\0')
+			{
+				z++;
+				y = 0;
+				break ;
+			}	
+			y++;
+		}
+		z++;
+	}
+}
+
+static void	built_in_unset_two(t_data *data, t_list *tmp, t_list *p_tmp, int i)
+{	
+	while (ft_strncmp(tmp->next->env_copy, data->argz[i],
+			ft_strlen(data->argz[i])) != 0)
+	{
+		tmp = tmp->next;
+		if (tmp->next == NULL)
+		{
+			tmp = NULL;
+			g_err_code = 0;
+			free(tmp);
+			return ;
+		}	
+	}
+	p_tmp = tmp->next;
+	tmp->next = p_tmp->next;
+	g_err_code = 0;
+	free(p_tmp->env_copy);
+	free(p_tmp);
+}
+
+static void	first_check_unset(t_data *data, int i)
+{
+	ft_putstr_fd(data->argz[i], 2);
+	ft_putstr_fd(" : not a valid identifier\n", 2);
+	g_err_code = 1;
+}
+
+void	built_in_unset(t_data *data, int i)
+{
+	t_list	*tmp;
+	t_list	*p_tmp;
+
+	if (data->argz[1] == NULL)
+		return ;
+	tmp = data->env;
+	p_tmp = NULL;
+	while (data->argz[i])
+	{
+		if (ft_isalpha(data->argz[i][0]) == 0 || data->argz[i][0] == '/')
+		{
+			first_check_unset(data, i);
+			return ;
+		}
+		if (ft_strncmp(tmp->env_copy, data->argz[i],
+				ft_strlen(data->argz[i])) == 0)
+		{
+			data->env = data->env->next;
+			free(tmp->env_copy);
+			free(tmp);
+		}
+		else
+			built_in_unset_two(data, tmp, p_tmp, i);
+		i++;
+	}
+}
---
+++ src/childs.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   childs.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agoichon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 11:23:06 by agoichon          #+#    #+#             */
+<<<<<<< HEAD
+/*   Updated: 2023/03/15 15:44:48 by jtoulous         ###   ########.fr       */
+=======
+/*   Updated: 2023/03/15 15:02:03 by agoichon         ###   ########.fr       */
+>>>>>>> 37e258b60fbd0ef11561946012444c0df385998e
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	simple_child(t_data *data)
+{
+	if (data->infile != -1)
+		dup2(data->infile, STDIN_FILENO);
+	if (data->outfile != -1)
+		dup2(data->outfile, STDOUT_FILENO);
+	close_all(data);
+	if (built_in(data) == -1)
+		execve(data->argz[0], data->argz, data->envp);
+	free_loop(data->envp);
+	free_and_close_all(data, 3);
+	mega_close(0, 1, 2, -1);
+	exit (0);
+}
+
+void	first_child(t_data *data)
+{
+	if (data->infile != -1)
+		dup2(data->infile, STDIN_FILENO);
+	if (data->outfile != -1)
+		dup2(data->outfile, STDOUT_FILENO);
+	else
+		dup2(data->pipes[0][1], STDOUT_FILENO);
+	close_all(data);
+	if (built_in(data) == -1)
+		execve(data->argz[0], data->argz, data->envp);
+	free_and_close_all(data, 3);
+	free_loop(data->envp);
+	mega_close(0, 1, 2, -1);
+	exit (0);
+}
+
+void	last_child(t_data *data, int z)
+{
+	if (data->prev_outfile != -1)
+		dup2(data->prev_outfile, STDIN_FILENO);
+	else if (data->infile != -1)
+		dup2(data->infile, STDIN_FILENO);
+	else
+		dup2(data->pipes[z - 1][0], STDIN_FILENO);
+	if (data->outfile != -1)
+		dup2(data->outfile, STDOUT_FILENO);
+	close_all(data);
+	if (built_in(data) == -1)
+		execve(data->argz[0], data->argz, data->envp);
+	free_and_close_all(data, 3);
+	free_loop(data->envp);
+	mega_close(0, 1, 2, -1);
+	exit (0);
+}
+
+void	multiple_child(t_data *data, int z)
+{
+	if (data->prev_outfile != -1)
+		dup2(data->prev_outfile, STDIN_FILENO);
+	else if (data->infile != -1)
+		dup2(data->infile, STDIN_FILENO);
+	else
+		dup2(data->pipes[z - 1][0], STDIN_FILENO);
+	if (data->outfile != -1)
+		dup2(data->outfile, STDOUT_FILENO);
+	else
+		dup2(data->pipes[z][1], STDOUT_FILENO);
+	close_all(data);
+	if (built_in(data) == -1)
+		execve(data->argz[0], data->argz, data->envp);
+	free_and_close_all(data, 3);
+	free_loop(data->envp);
+	mega_close(0, 1, 2, -1);
+	exit (0);
+}
